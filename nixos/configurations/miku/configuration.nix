1610acf8f98ae55847bee3fcac73f33ff76d6516
Treelar <me@treelar.xyz>
2025-07-27 08:57:25 -0700

    Update configuration.nix

--- nixos/configurations/miku/configuration.nix
+++ nixos/configurations/miku/configuration.nix
@@ -182,7 +182,7 @@
 
   system.autoUpgrade = {
     enable = true;
-    flake = "git+ssh://git@github.com/ninjawarrior1337/flake?ref=master";
+    flake = "git+ssh://git@github.com/ninjawarrior1337/flake";
     dates = "3:00";
 
     allowReboot = true;
