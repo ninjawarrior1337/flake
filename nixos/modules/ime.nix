994b7a799ec2e44317a95409cb158a3e27809428
Treelar <me@treelar.xyz>
2026-03-25 17:01:58 -0700

    unset kde pannel

--- nixos/modules/ime.nix
+++ nixos/modules/ime.nix
@@ -2,7 +2,7 @@
   i18n.inputMethod = {
     enable = true;
     type = "ibus";
-    ibus.panel = "${pkgs.kdePackages.plasma-desktop}/libexec/kimpanel-ibus-panel";
+    # ibus.panel = "${pkgs.kdePackages.plasma-desktop}/libexec/kimpanel-ibus-panel";
     ibus.engines = with pkgs.ibus-engines; [
       mozc-ut
     ];
