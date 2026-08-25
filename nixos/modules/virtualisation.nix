bb661dacc60d2023e1dc9ecd070f9c1143d47d35
Treelar <me@treelar.xyz>
2026-08-25 08:12:23 -0700

    modify virtualisation.nix

--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -1,8 +1,18 @@
 {
   user,
   pkgs,
+  config,
   ...
 }: {
+  # vm-curator fixes
+
+  systemd.tmpfiles.rules = let
+    fw = "${config.virtualisation.libvirtd.qemu.package}/share/qemu";
+  in [
+    "L /run/libvirt/nix-ovmf/OVMF_CODE.fd - - - - ${fw}/edk2-x86_64-code.fd"
+    "L /run/libvirt/nix-ovmf/OVMF_VARS.fd - - - - ${fw}/edk2-i386-vars.fd"
+  ];
+
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
@@ -13,7 +23,7 @@
   };
 
   users.users.${user} = {
-    extraGroups = [ "libvirtd" ];
+    extraGroups = ["libvirtd"];
   };
 
   environment.systemPackages = [
