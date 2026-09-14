e42111d3b95416c9cd008a267eff52ba13362fc1
Treelar <me@treelar.xyz>
2026-09-13 19:52:36 -0700

    update helium: 0.15.7.1 -> 0.17.0.1

--- packages/helium.nix
+++ packages/helium.nix
@@ -6,12 +6,12 @@
   runCommand,
   commandLineArgs ? [],
 }: let
-  version = "0.15.7.1";
+  version = "0.17.0.1";
   pname = "helium";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    hash = "sha256-+zGEGfhIiZWE8mUYb3HrkoM7reFBMdfXlgw3KWT0T98=";
+    hash = "sha256-JmqdEwoXP/2GGAMS2gjAq7G2oWFuyUTr5ouMDhSOcHY=";
   };
   appimageContents = appimageTools.extract {inherit pname version src;};
   unwrapped = appimageTools.wrapType2 {
