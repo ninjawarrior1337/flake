fa5636c6d77c5f3b1ffbe9332e7931c1eb4371a7
Treelar <me@treelar.xyz>
2026-09-22 14:07:15 -0700

    Update zen-browser flake to new owner
    Add home-manager input to zen-browser and update to latest revision from
    0xc000022070

--- flake.nix
+++ flake.nix
@@ -14,8 +14,9 @@
 
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
-    zen-browser.url = "github:youwen5/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    zen-browser.inputs.home-manager.follows = "home-manager";
 
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
